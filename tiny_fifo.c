651fbbf47a6f9cfae45f5f4abb8f314324ea94a6
LiWei <96402566+HexSDR@users.noreply.github.com>
2023-09-10 22:28:33 +0800

    Update tiny_fifo.c

--- tiny_fifo.c
+++ tiny_fifo.c
@@ -1,15 +1,16 @@
 
+#include <pthread.h>
+#include <semaphore.h>
 
-#typedef  int  FIFO_NODE_TYPE
+typedef  int  FIFO_NODE_TYPE
 #define MAX_BIT  7
 #define MAX_SIZE (1<<MAX_BIT)
 
-struct ts_fifo 
-{
+struct ts_fifo {
     pthread_mutex_t fifo_lock ;
-    int wr_ptr,
-    int rd_ptr,
-    int gap,
+    int wr_ptr;
+    int rd_ptr;
+    int gap;
     FIFO_NODE_TYPE buff[MAX_SIZE]
 };
 
@@ -23,7 +24,6 @@ void init_ts_fifo(struct ts_fifo*p)
     pthread_mutex_unlock(&(p->fifo_lock));
     
 }
-
 
 void push(struct ts_fifo*p,FIFO_NODE_TYPE d)
 {
@@ -43,8 +43,7 @@ int is_fifo_empty(struct ts_fifo*p)
     r=(p->gap==0);
     pthread_mutex_unlock(&(p->fifo_lock));
     return r ;
-}
-//#define is_fifo_full() (gap==MAX_SIZE )
+} 
 unsigned int is_fifo_full(struct ts_fifo*p)
 {
     int r ;
@@ -54,7 +53,7 @@ unsigned int is_fifo_full(struct ts_fifo*p)
     return r ;
 }
 
-FIFO_NODE_TYPE pop(struct ts_fifo*p)
+FIFO_NODE_TYPE pop(void)
 {
     NODE_TYPE r 
     pthread_mutex_lock(&(p->fifo_lock));
@@ -65,3 +64,5 @@ FIFO_NODE_TYPE pop(struct ts_fifo*p)
     
     return r ;
 }
+
+ 
