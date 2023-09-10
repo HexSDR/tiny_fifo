9f5ddacc349c875b644ab0a111ea4d55d989a135
LiWei <96402566+HexSDR@users.noreply.github.com>
2023-09-10 22:32:07 +0800

    Update tiny_fifo.c

--- tiny_fifo.c
+++ tiny_fifo.c
@@ -2,18 +2,6 @@
 #include <pthread.h>
 #include <semaphore.h>
 
-typedef  int  FIFO_NODE_TYPE
-#define MAX_BIT  7
-#define MAX_SIZE (1<<MAX_BIT)
-
-struct ts_fifo {
-    pthread_mutex_t fifo_lock ;
-    int wr_ptr;
-    int rd_ptr;
-    int gap;
-    FIFO_NODE_TYPE buff[MAX_SIZE]
-};
-
 
 void init_ts_fifo(struct ts_fifo*p)
 {
